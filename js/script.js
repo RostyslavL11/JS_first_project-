0c435c30b7c8184ddf1006032424df61dd951e23
Rostik <RostikL1994@gmail.com>
2021-08-03 18:23:23 +0300

    third commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+
+console.log(4*2 === 8);
+
+console.log('arr' + " - object")
+console.log(5 + +"5")
+
+
+console.log(2 + 2*2 !== '6');
+
+
+
+const isCheked = false,
+      isClosed = false;
+
+console.log(isCheked || !isClosed); 
